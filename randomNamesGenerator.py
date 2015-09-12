33c291d53007a1d7e076d2affb36c8ab64b632a6
dealen <marcickiewiczjakub@gmail.com>
2015-09-13 00:59:22 +0200

    2. Random Names Generator

--- randomNamesGenerator.py
+++ randomNamesGenerator.py
@@ -1,17 +1,17 @@
 from random import choice
 
-class RandomName():
 
-	list1 = ['Ar', 'Gor', 'Mak', 'Vlo', 'Dur', 'Mur'];
-	list2 = ['nak', 'vok', 'nik', 'stu', 'an', 'bin'];
+class RandomName:
+    list1 = ["Ar", "Gor", "Mak", "Vlo", "Dur", "Mur"]
+    list2 = ["nak", "vok", "nik", "stu", "an", "bin"]
 
-	def GenerateRandomName(self):
-		val1 = choice(self.list1)
-		val2 = choice(self.list2)
-		
-		return val1 + val2;
+    def generate_random_name(self):
+        val1 = choice(self.list1)
+        val2 = choice(self.list2)
+        return val1 + val2
+
 
 rand = RandomName()
-result = rand.GenerateRandomName()
+result = rand.generate_random_name()
 
 print(result)
